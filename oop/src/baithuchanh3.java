556da14da335e21c8baf6526a59bf29220110399
duchoangcodegym <vuhoang231205@gmail.com>
2024-04-08 00:06:46 +0700

    baitap

--- oop/src/baithuchanh3.java
+++ oop/src/baithuchanh3.java
@@ -7,13 +7,12 @@ public class baithuchanh3 {
         float width;
         float height;
 
-        Scanner scanner = new Scanner(System.in);//Khai báo đối tượng Scanner
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter width: ");
-        width = scanner.nextFloat();//Nhập chiều rộng
-
+        width = scanner.nextFloat();
         System.out.println("Enter height: ");
-        height = scanner.nextFloat();//Nhập chiều dài
+        height = scanner.nextFloat();
         float area = width * height;
 
         System.out.println("Area is: " + area);
